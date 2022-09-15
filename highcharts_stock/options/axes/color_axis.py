ee432ce6779b8d5964bb1aa24fdcaa1de9b84230
Chris Modzelewski <software@insightindustry.com>
2022-09-15 17:08:08 -0400

    Implemented highcharts_stock.axes.

---
+++ highcharts_stock/options/axes/color_axis.py
@@ -0,0 +1,198 @@
+from typing import Optional
+from decimal import Decimal
+
+from validator_collection import validators
+
+from highcharts_python.options.axes.color_axis import ColorAxis as ColorAxisBase
+
+
+class ColorAxis(ColorAxisBase):
+
+    def __init__(self, **kwargs):
+        self._max_range = None
+        self._overscroll = None
+        self._range = None
+        self._resize = None
+
+        self.max_range = kwargs.get('max_range', None)
+        self.overscroll = kwargs.get('overscroll', None)
+        self.range = kwargs.get('range', None)
+        self.resize = kwargs.get('resize', None)
+
+        super().__init__(**kwargs)
+
+    @property
+    def max_range(self) -> Optional[int | float | Decimal]:
+        """Maximum range which can be set using the navigator's handles. Defaults to
+        :obj:`None <python:None>`.
+
+        .. note::
+
+          This setting is the opposite of
+          :meth:`.min_range <Highcharts_Stock.options.axes.color_axis.ColorAxis.min_range>`
+
+        :rtype: numeric or :obj:`None <python:None>`
+        """
+        return self._max_range
+
+    @max_range.setter
+    def max_range(self, value):
+        self._max_range = validators.numeric(value, allow_empty = True)
+
+    @property
+    def overscroll(self) -> Optional[int]:
+        """Additional range on the right side of the x-axis. Works similar to
+        :meth:`.max_padding <highcharts_stock.options.axes.color_axis.ColorAxis.max_padding>`, but
+        value is set in milliseconds. Defaults to ``0``.
+
+        :rtype: :class:`int <python:int>` or :obj:`None <python:None>`
+        """
+        return self._overscroll
+
+    @overscroll.setter
+    def overscroll(self, value):
+        self._overscroll = validators.integer(value,
+                                              allow_empty = True,
+                                              minimum = 0)
+
+    @property
+    def range(self) -> Optional[int | float | Decimal]:
+        """The minimum range to display on this axis. Defaults to
+        :obj:`None <python:None>`.
+
+        .. note::
+
+          The entire axis will not be allowed to span over a smaller interval than this.
+          For example, for a datetime axis the main unit is milliseconds. If ``min_range``
+          is set to ``3600000``, you can't zoom in more than to one hour.
+
+          On a logarithmic axis, the unit for the range is the power. So a ``range`` of
+          ``1`` means that the axis can be zoomed to 10-100, 100-1,000, 1,000-10,000 etc.
+
+        .. note::
+
+          For the x-axis, when :obj:`None <python:None>`, defaults to five times the
+          smallest interval between any of the data points.
+
+        .. caution::
+
+          The :meth:`min_padding <highcharts_stock.options.axes.color_axis.ColorAxis.min_padding>`,
+          :meth:`max_padding <highcharts_stock.options.axes.color_axis.ColorAxis.max_padding>`,
+          :meth:`start_on_tick <highcharts_stock.options.axes.color_axis.ColorAxis.start_on_tick>`,
+          and :meth:`end_on_tick <highcharts_stock.options.axes.color_axis.ColorAxis.end_on_tick>`
+          settings also affect how the extremes of the axis are computed.
+
+        :rtype: numeric or :obj:`None <python:None>`
+        """
+        return self._range
+
+    @range.setter
+    def range(self, value):
+        self._range = validators.numeric(value, allow_empty = True)
+
+    @classmethod
+    def _get_kwargs_from_dict(cls, as_dict):
+        kwargs = {
+            'accessibility': as_dict.get('accessibility', None),
+            'align_ticks': as_dict.get('alignTicks', None),
+            'allow_decimals': as_dict.get('allowDecimals', None),
+            'alternate_grid_color': as_dict.get('alternateGridColor', None),
+            'angle': as_dict.get('angle', None),
+            'breaks': as_dict.get('breaks', None),
+            'categories': as_dict.get('categories', None),
+            'ceiling': as_dict.get('ceiling', None),
+            'class_name': as_dict.get('className', None),
+            'date_time_label_formats': as_dict.get('dateTimeLabelFormats', None),
+            'end_on_tick': as_dict.get('endOnTick', None),
+            'events': as_dict.get('events', None),
+            'floor': as_dict.get('floor', None),
+            'grid_line_color': as_dict.get('gridLineColor', None),
+            'grid_line_dash_style': as_dict.get('gridLineDashStyle', None),
+            'grid_line_interpolation': as_dict.get('gridLineInterpolation', None),
+            'grid_line_width': as_dict.get('gridLineWidth', None),
+            'grid_z_index': as_dict.get('gridZIndex', None),
+            'id': as_dict.get('id', None),
+            'labels': as_dict.get('labels', None),
+            'linked_to': as_dict.get('linkedTo', None),
+            'margin': as_dict.get('margin', None),
+            'max': as_dict.get('max', None),
+            'max_padding': as_dict.get('maxPadding', None),
+            'min': as_dict.get('min', None),
+            'minor_grid_line_color': as_dict.get('minorGridLineColor', None),
+            'minor_grid_line_dash_style': as_dict.get('minorGridLineDashStyle', None),
+            'minor_grid_line_width': as_dict.get('minorGridLineWidth', None),
+            'minor_tick_color': as_dict.get('minorTickColor', None),
+            'minor_tick_interval': as_dict.get('minorTickInterval', None),
+            'minor_tick_length': as_dict.get('minorTickLength', None),
+            'minor_tick_position': as_dict.get('minorTickPosition', None),
+            'minor_ticks': as_dict.get('minorTicks', None),
+            'minor_tick_width': as_dict.get('minorTickWidth', None),
+            'min_padding': as_dict.get('minPadding', None),
+            'min_range': as_dict.get('minRange', None),
+            'min_tick_interval': as_dict.get('minTickInterval', None),
+            'offset': as_dict.get('offset', None),
+            'opposite': as_dict.get('opposite', None),
+            'pane': as_dict.get('pane', None),
+            'panning_enabled': as_dict.get('panningEnabled', None),
+            'plot_bands': as_dict.get('plotBands', None),
+            'plot_lines': as_dict.get('plotLines', None),
+            'reversed': as_dict.get('reversed', None),
+            'reversed_stacks': as_dict.get('reversedStacks', None),
+            'show_first_label': as_dict.get('showFirstLabel', None),
+            'show_last_label': as_dict.get('showLastLabel', None),
+            'soft_max': as_dict.get('softMax', None),
+            'soft_min': as_dict.get('softMin', None),
+            'start_of_week': as_dict.get('startOfWeek', None),
+            'start_on_tick': as_dict.get('startOnTick', None),
+            'tick_amount': as_dict.get('tickAmount', None),
+            'tick_color': as_dict.get('tickColor', None),
+            'tick_interval': as_dict.get('tickInterval', None),
+            'tick_length': as_dict.get('tickLength', None),
+            'tickmark_placement': as_dict.get('tickmarkPlacement', None),
+            'tick_pixel_interval': as_dict.get('tickPixelInterval', None),
+            'tick_position': as_dict.get('tickPosition', None),
+            'tick_positioner': as_dict.get('tickPositioner', None),
+            'tick_positions': as_dict.get('tickPositions', None),
+            'tick_width': as_dict.get('tickWidth', None),
+            'title': as_dict.get('title', None),
+            'type': as_dict.get('type', None),
+            'unique_names': as_dict.get('uniqueNames', None),
+            'units': as_dict.get('units', None),
+            'visible': as_dict.get('visible', None),
+            'z_index': as_dict.get('zIndex', None),
+            'zoom_enabled': as_dict.get('zoomEnabled', None),
+
+            'crosshair': as_dict.get('crosshair', None),
+            'height': as_dict.get('height', None),
+            'left': as_dict.get('left', None),
+            'line_color': as_dict.get('lineColor', None),
+            'line_width': as_dict.get('lineWidth', None),
+            'show_empty': as_dict.get('showEmpty', None),
+            'top': as_dict.get('top', None),
+            'width': as_dict.get('width', None),
+
+            'max_color': as_dict.get('maxColor', None),
+            'min_color': as_dict.get('minColor', None),
+            'stack_labels': as_dict.get('stackLabels', None),
+            'stops': as_dict.get('stops', None),
+            'tooltip_value_format': as_dict.get('tooltipValueFormat', None),
+
+            'max_range': as_dict.get('maxRange', None),
+            'overscroll': as_dict.get('overscroll', None),
+            'range': as_dict.get('range', None),
+        }
+
+        return kwargs
+
+    def _to_untrimmed_dict(self, in_cls = None) -> dict:
+        untrimmed = {
+            'maxRange': self.max_range,
+            'overscroll': self.overscroll,
+            'range': self.range,
+        }
+
+        parent_as_dict = super()._to_untrimmed_dict(in_cls = in_cls)
+        for key in parent_as_dict:
+            untrimmed[key] = parent_as_dict[key]
+
+        return untrimmed
---
+++ highcharts_stock/options/axes/resize.py
@@ -0,0 +1,275 @@
+from typing import Optional, List
+from decimal import Decimal
+
+from validator_collection import validators
+
+from highcharts_python import constants
+from highcharts_python.decorators import class_sensitive
+from highcharts_python.metaclasses import HighchartsMeta
+
+from highcharts_stock import errors
+
+
+class ControlledAxis(HighchartsMeta):
+    """Axes that are controlled by the control line of the axis."""
+
+    def __init__(self, **kwargs):
+        self._next = None
+        self._prev = None
+
+        self.next = kwargs.get('next', None)
+        self.prev = kwargs.get('prev', None)
+
+    @property
+    def next(self) -> Optional[List[int | str]]:
+        """Array of axes that should move out of the way of resizing being done for the
+        current axis. If not set, the next axis will be used. If
+        :obj:`None <python:None>`, defaults to an empty array.
+
+        :rtype: :class:`list <python:list>` of :class:`int <python:int>` or
+          :class:`str <python:str>`, or :class:`None <python:None>`
+        """
+        return self._next
+
+    @next.setter
+    def next(self, value):
+        if value is None:
+            self._next = None
+        else:
+            value = validators.iterable(value, allow_empty = True)
+            processed_value = []
+            for item in value:
+                try:
+                    item = validators.integer(item)
+                except (ValueError, TypeError):
+                    item = validators.string(item)
+
+                processed_value.append(item)
+
+            self._next = [x for x in processed_value]
+
+    @property
+    def prev(self) -> Optional[List[int | str]]:
+        """Array of axes that should move with the current axis during resizing.
+
+        :rtype: :class:`list <python:list>` of :class:`int <python:int>` or
+          :class:`str <python:str>`, or :class:`None <python:None>`
+        """
+        return self._prev
+
+    @prev.setter
+    def prev(self, value):
+        if value is None:
+            self._prev = None
+        else:
+            value = validators.iterable(value, allow_empty = True)
+            processed_value = []
+            for item in value:
+                try:
+                    item = validators.integer(item)
+                except (ValueError, TypeError):
+                    item = validators.string(item)
+
+                processed_value.append(item)
+
+            self._prev = [x for x in processed_value]
+
+    @classmethod
+    def _get_kwargs_from_dict(cls, as_dict):
+        kwargs = {
+            'next': as_dict.get('next', None),
+            'prev': as_dict.get('prev', None),
+        }
+
+        return kwargs
+
+    def _to_untrimmed_dict(self, in_cls = None) -> dict:
+        untrimmed = {
+            'next': self.next,
+            'prev': selfprev,
+        }
+
+        return untrimmed
+
+
+class ResizeOptions(HighchartsMeta):
+    """Options for axis resizing.
+
+    .. note::
+
+      It adds a thick line between panes which the user can drag in order to resize the
+      panes.
+
+    """
+
+    def __init__(self, **kwargs):
+        self._controlled_axis = None
+        self._cursor = None
+        self._enabled = None
+        self._line_color = None
+        self._line_dash_style = None
+        self._line_width = None
+        self._x = None
+        self._y = None
+
+        self.controlled_axis = kwargs.get('controlled_axis', None)
+        self.cursor = kwargs.get('cursor', None)
+        self.enabled = kwargs.get('enabled', None)
+        self.line_color = kwargs.get('line_color', None)
+        self.line_dash_style = kwargs.get('line_dash_style', None)
+        self.line_width = kwargs.get('line_width', None)
+        self.x = kwargs.get('x', None)
+        self.y = kwargs.get('y', None)
+
+    @property
+    def controlled_axis(self) -> Optional[ControlledAxis]:
+        """Axes that are controlled by the control line of the axis.
+
+        :rtype: :class:`ControlledAxis` or :obj:`None <python:None>`
+        """
+        return self._controlled_axis
+
+    @controlled_axis.setter
+    @class_sensitive(ControlledAxis)
+    def controlled_axis(self, value):
+        self._controlled_axis = value
+
+    @property
+    def cursor(self) -> Optional[str]:
+        """Cursor style for the control line. Defaults to ``'ns-resize'``.
+
+        :rtype: :class:`str <python:str>` or :obj:`None <python:None>`
+        """
+        return self._cursor
+
+    @cursor.setter
+    def cursor(self, value):
+        self._cursor = validators.string(value, allow_empty = True)
+
+    @property
+    def enabled(self) -> Optional[bool]:
+        """If ``True``, enable resizing of the axis by dragging. Defaults to ``False``.
+
+        :rtype: :class:`bool <python:bool>` or :obj:`None <python:None>`
+        """
+        return self._enabled
+
+    @enabled.setter
+    def enabled(self, value):
+        if value is None:
+            self._enabled = None
+        else:
+            self._enabled = bool(value)
+
+    @property
+    def line_color(self) -> Optional[str]:
+        """Color of the control line. Defaults to ``'#cccccc'.
+
+        :rtype: :class:`str <python:str>` or :obj:`None <python:None>`
+        """
+        return self._line_color
+
+    @line_color.setter
+    def line_color(self, value):
+        self._line_color = validators.string(value, allow_empty = True)
+
+    @property
+    def line_dash_style(self) -> Optional[str]:
+        """Name of the dash style to use for the control line. Defaults to ``'Solid'``.
+
+        Accepts the following values:
+
+          * ``'Solid'``
+          * ``'ShortDash'``
+          * ``'ShortDot'``
+          * ``'ShortDashDot'``
+          * ``'ShortDashDotDot'``
+          * ``'Dot'``
+          * ``'Dash'``
+          * ``'LongDash'``
+          * ``'DashDot'``
+          * ``'LongDashDot'``
+          * ``'LongDashDotDot'``
+
+        :returns: The name of the dash style to apply to the control line.
+        :rtype: :class:`str <python:str>` or :obj:`None <python:None>`
+        """
+        return self._line_dash_style
+
+    @line_dash_style.setter
+    def line_dash_style(self, value):
+        value = validators.string(value, allow_empty = True)
+        if not value:
+            self._dash_style = None
+        else:
+            if value not in constants.SHAPES_ALLOWED_DASH_STYLES:
+                raise errors.HighchartsValueError(f'dash_style expects a supported value.'
+                                                  f' Received: {value}')
+            self._line_dash_style = value
+
+    @property
+    def line_width(self) -> Optional[int | float | Decimal]:
+        """Pixel width of the control line. Defaults to ``4``.
+
+        :rtype: numeric or :obj:`None <python:None>`
+        """
+        return self._line_width
+
+    @line_width.setter
+    def line_width(self, value):
+        self._line_width = validators.numeric(value,
+                                              allow_empty = True,
+                                              minimum = 0)
+
+    @property
+    def x(self) -> Optional[int | float | Decimal]:
+        """Horizontal offset of the control line. Defaults to ``0``.
+
+        :rtype: numeric or :obj:`None <python:None>`
+        """
+        return self._x
+
+    @x.setter
+    def x(self, value):
+        self._x = validators.numeric(value, allow_empty = True)
+
+    @property
+    def y(self) -> Optional[int | float | Decimal]:
+        """Vertical offset of the control line. Defaults to ``0``.
+
+        :rtype: numeric or :obj:`None <python:None>`
+        """
+        return self._y
+
+    @y.setter
+    def y(self, value):
+        self._y = validators.numeric(value, allow_empty = True)
+
+    @classmethod
+    def _get_kwargs_from_dict(cls, as_dict):
+        kwargs = {
+            'controlled_axis': as_dict.get('controlledAxis', None),
+            'cursor': as_dict.get('cursor', None),
+            'enabled': as_dict.get('enabled', None),
+            'line_color': as_dict.get('lineColor', None),
+            'line_dash_style': as_dict.get('lineDashStyle', None),
+            'line_width': as_dict.get('lineWidth', None),
+            'x': as_dict.get('x', None),
+            'y': as_dict.get('y', None),
+        }
+
+        return kwargs
+
+    def _to_untrimmed_dict(self, in_cls = None) -> dict:
+        untrimmed = {
+            'controlledAxis': self.controlled_axis,
+            'cursor': self.cursor,
+            'enabled': self.enabled,
+            'lineColor': self.line_color,
+            'lineDashStyle': self.line_dash_style,
+            'lineWidth': self.line_width,
+            'x': self.x,
+            'y': self.y,
+        }
+
+        return untrimmed
