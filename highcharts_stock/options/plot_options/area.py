503534f512b0b511bcfb0b3d50e0d76297edaf29
Chris Modzelewski <software@insightindustry.com>
2022-09-19 14:22:33 -0400

    Implemented options.plot_options.candlestick.CandlestickOptions.

--- highcharts_stock/options/plot_options/area.py
+++ highcharts_stock/options/plot_options/area.py
@@ -1,4 +1,7 @@
-from highcharts_python.options.plot_options.area import AreaOptions as AreaBase
+from typing import Optional
+
+from highcharts_python.options.plot_options.area import AreaOptions as AreaBase, \
+                                                        LineOptions as LineBase
 from highcharts_python.utility_functions import mro__to_untrimmed_dict
 
 from highcharts_stock.options.plot_options.base import NonIndicatorOptions
@@ -14,10 +17,6 @@ class AreaOptions(AreaBase, NonIndicatorOptions):
     """
 
     def __init__(self, **kwargs):
-        self._point_range = None
-
-        self.point_range = kwargs.get('point_range', None)
-
         super().__init__(**kwargs)
 
     @classmethod
@@ -151,7 +150,7 @@ class AreaSplineRangeOptions(AreaOptions):
     pass
 
 
-class LineOptions(AreaOptions):
+class LineOptions(LineBase, NonIndicatorOptions):
     """General options to apply to all Line series types.
 
     A line series displays information as a series of data points connected by
@@ -162,7 +161,129 @@ class LineOptions(AreaOptions):
       :align: center
 
     """
-    pass
+
+    def __init__(self, **kwargs):
+        self._use_ohlc_data = None
+
+        self.use_ohlc_data = kwargs.get('use_ohlc_data', None)
+
+    @property
+    def use_ohlc_data(self) -> Optional[bool]:
+        """If ``True``, generates a Line :term:`series` from :term:`OHLC`-style data.
+        Defaults to :obj:`None <python:None>`.
+
+        .. caution::
+
+          Data in OHLC-format is required for this to function properly if set to
+          ``True``.
+
+        :rtype: :class:`bool <python:bool>` or :obj:`None <python:None>`
+        """
+        return self._use_ohlc_data
+
+    @use_ohlc_data.setter
+    def use_ohlc_data(self, value):
+        if value is None:
+            self._use_ohlc_data = None
+        else:
+            self._use_ohlc_data = bool(value)
+
+    @classmethod
+    def _get_kwargs_from_dict(cls, as_dict):
+        kwargs = {
+            'accessibility': as_dict.get('accessibility', None),
+            'allow_point_select': as_dict.get('allowPointSelect', None),
+            'animation': as_dict.get('animation', None),
+            'class_name': as_dict.get('className', None),
+            'clip': as_dict.get('clip', None),
+            'color': as_dict.get('color', None),
+            'cursor': as_dict.get('cursor', None),
+            'custom': as_dict.get('custom', None),
+            'dash_style': as_dict.get('dashStyle', None),
+            'data_labels': as_dict.get('dataLabels', None),
+            'description': as_dict.get('description', None),
+            'enable_mouse_tracking': as_dict.get('enableMouseTracking', None),
+            'events': as_dict.get('events', None),
+            'include_in_data_export': as_dict.get('includeInDataExport', None),
+            'keys': as_dict.get('keys', None),
+            'label': as_dict.get('label', None),
+            'linked_to': as_dict.get('linkedTo', None),
+            'marker': as_dict.get('marker', None),
+            'on_point': as_dict.get('onPoint', None),
+            'opacity': as_dict.get('opacity', None),
+            'point': as_dict.get('point', None),
+            'point_description_formatter': as_dict.get('pointDescriptionFormatter', None),
+            'selected': as_dict.get('selected', None),
+            'show_checkbox': as_dict.get('showCheckbox', None),
+            'show_in_legend': as_dict.get('showInLegend', None),
+            'skip_keyboard_navigation': as_dict.get('skipKeyboardNavigation', None),
+            'states': as_dict.get('states', None),
+            'sticky_tracking': as_dict.get('stickyTracking', None),
+            'threshold': as_dict.get('threshold', None),
+            'tooltip': as_dict.get('tooltip', None),
+            'turbo_threshold': as_dict.get('turboThreshold', None),
+            'visible': as_dict.get('visible', None),
+
+            'animation_limit': as_dict.get('animationLimit', None),
+            'boost_blending': as_dict.get('boostBlending', None),
+            'boost_threshold': as_dict.get('boostThreshold', None),
+            'color_axis': as_dict.get('colorAxis', None),
+            'color_index': as_dict.get('colorIndex', None),
+            'color_key': as_dict.get('colorKey', None),
+            'connect_ends': as_dict.get('connectEnds', None),
+            'connect_nulls': as_dict.get('connectNulls', None),
+            'crisp': as_dict.get('crisp', None),
+            'crop_threshold': as_dict.get('cropThreshold', None),
+            'data_sorting': as_dict.get('dataSorting', None),
+            'drag_drop': as_dict.get('dragDrop', None),
+            'find_nearest_point_by': as_dict.get('findNearestPointBy', None),
+            'get_extremes_from_all': as_dict.get('getExtremesFromAll', None),
+            'linecap': as_dict.get('linecap', None),
+            'line_width': as_dict.get('lineWidth', None),
+            'negative_color': as_dict.get('negativeColor', None),
+            'point_interval': as_dict.get('pointInterval', None),
+            'point_interval_unit': as_dict.get('pointIntervalUnit', None),
+            'point_placement': as_dict.get('pointPlacement', None),
+            'point_start': as_dict.get('pointStart', None),
+            'relative_x_value': as_dict.get('relativeXValue', None),
+            'shadow': as_dict.get('shadow', None),
+            'soft_threshold': as_dict.get('softThreshold', None),
+            'stacking': as_dict.get('stacking', None),
+            'step': as_dict.get('step', None),
+            'zone_axis': as_dict.get('zoneAxis', None),
+            'zones': as_dict.get('zones', None),
+
+            'compare_start': as_dict.get('compareStart', None),
+            'cumulative': as_dict.get('cumulative', None),
+            'data_as_columns': as_dict.get('dataAsColumns', None),
+            'data_grouping': as_dict.get('dataGrouping', None),
+            'gap_size': as_dict.get('gapSize', None),
+            'gap_unit': as_dict.get('gapUnit', None),
+            'last_price': as_dict.get('lastPrice', None),
+            'last_visible_price': as_dict.get('lastVisiblePrice', None),
+
+            'compare': as_dict.get('compare', None),
+            'compare_base': as_dict.get('compareBase', None),
+            'navigator_options': as_dict.get('navigatorOptions', None),
+            'point_range': as_dict.get('pointRange', None),
+            'show_in_navigator': as_dict.get('showInNavigator', None),
+
+            'use_ohlc_data': as_dict.get('uesOhlcData', None),
+
+        }
+
+        return kwargs
+
+    def _to_untrimmed_dict(self, in_cls = None) -> dict:
+        untrimmed = {
+            'useOhlcData': self.use_ohlc_data,
+        }
+        parents_as_dict = mro__to_untrimmed_dict(self, in_cls = in_cls) or {}
+
+        for key in parents_as_dict:
+            untrimmed[key] = parents_as_dict[key]
+
+        return untrimmed
 
 
 class StreamGraphOptions(AreaOptions):
