e3e6c2da48ac3f13c79bd27e4bf306deca1fb248
Chris Modzelewski <software@insightindustry.com>
2022-09-19 14:31:15 -0400

    Implemented Dumbbell and related support.

---
+++ highcharts_stock/options/plot_options/dumbbell.py
@@ -0,0 +1,136 @@
+from highcharts_python.options.plot_options.dumbbell import \
+    DumbbellOptions as DumbbellBase
+from highcharts_python.utility_functions import mro__to_untrimmed_dict
+
+from highcharts_stock.options.plot_options.base import NonIndicatorOptions
+
+
+class DumbbellOptions(DumbbellBase, NonIndicatorOptions):
+    """General options to apply to all Dumbbell series types.
+
+    The dumbbell series is a cartesian series with higher and lower values for each
+    point along an X axis, connected with a line between the values.
+
+    .. figure:: ../../../_static/dumbbell-example.png
+      :alt: Dumbbell Example Chart
+      :align: center
+
+    """
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    @classmethod
+    def _get_kwargs_from_dict(cls, as_dict):
+        kwargs = {
+            'accessibility': as_dict.get('accessibility', None),
+            'allow_point_select': as_dict.get('allowPointSelect', None),
+            'animation': as_dict.get('animation', None),
+            'class_name': as_dict.get('className', None),
+            'clip': as_dict.get('clip', None),
+            'color': as_dict.get('color', None),
+            'cursor': as_dict.get('cursor', None),
+            'custom': as_dict.get('custom', None),
+            'dash_style': as_dict.get('dashStyle', None),
+            'data_labels': as_dict.get('dataLabels', None),
+            'description': as_dict.get('description', None),
+            'enable_mouse_tracking': as_dict.get('enableMouseTracking', None),
+            'events': as_dict.get('events', None),
+            'include_in_data_export': as_dict.get('includeInDataExport', None),
+            'keys': as_dict.get('keys', None),
+            'label': as_dict.get('label', None),
+            'linked_to': as_dict.get('linkedTo', None),
+            'marker': as_dict.get('marker', None),
+            'on_point': as_dict.get('onPoint', None),
+            'opacity': as_dict.get('opacity', None),
+            'point': as_dict.get('point', None),
+            'point_description_formatter': as_dict.get('pointDescriptionFormatter', None),
+            'selected': as_dict.get('selected', None),
+            'show_checkbox': as_dict.get('showCheckbox', None),
+            'show_in_legend': as_dict.get('showInLegend', None),
+            'skip_keyboard_navigation': as_dict.get('skipKeyboardNavigation', None),
+            'states': as_dict.get('states', None),
+            'sticky_tracking': as_dict.get('stickyTracking', None),
+            'threshold': as_dict.get('threshold', None),
+            'tooltip': as_dict.get('tooltip', None),
+            'turbo_threshold': as_dict.get('turboThreshold', None),
+            'visible': as_dict.get('visible', None),
+
+            'animation_limit': as_dict.get('animationLimit', None),
+            'color_axis': as_dict.get('colorAxis', None),
+            'color_index': as_dict.get('colorIndex', None),
+            'color_key': as_dict.get('colorKey', None),
+            'connect_ends': as_dict.get('connectEnds', None),
+            'connect_nulls': as_dict.get('connectNulls', None),
+            'connector_color': as_dict.get('connectorColor', None),
+            'connector_width': as_dict.get('connectorWidth', None),
+            'crisp': as_dict.get('crisp', None),
+            'crop_threshold': as_dict.get('cropThreshold', None),
+            'data_sorting': as_dict.get('dataSorting', None),
+            'drag_drop': as_dict.get('dragDrop', None),
+            'find_nearest_point_by': as_dict.get('findNearestPointBy', None),
+            'get_extremes_from_all': as_dict.get('getExtremesFromAll', None),
+            'group_padding': as_dict.get('groupPadding', None),
+            'linecap': as_dict.get('linecap', None),
+            'line_color': as_dict.get('lineColor', None),
+            'low_color': as_dict.get('lowColor', None),
+            'negative_color': as_dict.get('negativeColor', None),
+            'negative_fill_color': as_dict.get('negativeFillColor', None),
+            'point_interval': as_dict.get('pointInterval', None),
+            'point_interval_unit': as_dict.get('pointIntervalUnit', None),
+            'point_padding': as_dict.get('pointPadding', None),
+            'point_placement': as_dict.get('pointPlacement', None),
+            'point_start': as_dict.get('pointStart', None),
+            'relative_x_value': as_dict.get('relativeXValue', None),
+            'shadow': as_dict.get('shadow', None),
+            'soft_threshold': as_dict.get('softThreshold', None),
+            'stacking': as_dict.get('stacking', None),
+            'step': as_dict.get('step', None),
+            'zone_axis': as_dict.get('zoneAxis', None),
+            'zones': as_dict.get('zones', None),
+
+            'compare_start': as_dict.get('compareStart', None),
+            'cumulative': as_dict.get('cumulative', None),
+            'data_as_columns': as_dict.get('dataAsColumns', None),
+            'data_grouping': as_dict.get('dataGrouping', None),
+            'gap_size': as_dict.get('gapSize', None),
+            'gap_unit': as_dict.get('gapUnit', None),
+            'last_price': as_dict.get('lastPrice', None),
+            'last_visible_price': as_dict.get('lastVisiblePrice', None),
+
+            'compare': as_dict.get('compare', None),
+            'compare_base': as_dict.get('compareBase', None),
+            'navigator_options': as_dict.get('navigatorOptions', None),
+            'point_range': as_dict.get('pointRange', None),
+            'show_in_navigator': as_dict.get('showInNavigator', None),
+        }
+
+        return kwargs
+
+    def _to_untrimmed_dict(self, in_cls = None) -> dict:
+        untrimmed = {}
+        parent_as_dict = mro__to_untrimmed_dict(self, in_cls = in_cls) or {}
+
+        for key in parent_as_dict:
+            untrimmed[key] = parent_as_dict[key]
+
+        return untrimmed
+
+
+class LollipopOptions(DumbbellOptions):
+    """General options to apply to all Lollipop series types.
+
+    The lollipop series is a carteseian series with a line anchored from the x axis
+    and a dot at the end to mark the value.
+
+    .. warning::
+
+      Requires ``highcharts-more.js``, ``modules/dumbbell.js``, and
+      ``modules/lollipop.js`` to be loaded client-side.
+
+    .. figure:: ../../../_static/lollipop-example.png
+      :alt: Lollipop Example Chart
+      :align: center
+
+    """
+    pass
