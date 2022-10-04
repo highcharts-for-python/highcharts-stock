9b6e463ea04b45884d43cdbd1ae219641e1d8799
Chris Modzelewski <software@insightindustry.com>
2022-10-04 16:17:16 -0400

    Implemented unit tests for options.chart.zooming.

---
+++ tests/input_files/chart/zooming/01.js
@@ -0,0 +1,15 @@
+{
+  key: 'ctrl',
+  pinchType: 'xy',
+  resetButton: {
+    position: {
+        align: 'center',
+        verticalAlign: 'top',
+        x: -10,
+        y: 10
+    },
+    relativeTo: 'plot'
+  },
+  singleTouch: true,
+  type: 'xy'
+}
