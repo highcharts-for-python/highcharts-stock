589c07ba091cfa42086c7f8371053cc836c2d67f
Chris Modzelewski <chris@highchartspython.com>
2023-04-01 13:17:43 -0400

    Fixed module inclusions.

--- highcharts_stock/constants.py
+++ highcharts_stock/constants.py
@@ -1,27 +1,23 @@
 """Defines a set of constants that are used throughout the library."""
 from highcharts_core.constants import *
 
-STOCK_INCLUDE_LIBS = [
-    "https://code.highcharts.com/stock/highstock.js",
-    "https://code.highcharts.com/stock/highstock.src.js",
-    "https://code.highcharts.com/stock/highcharts-more.js",
-    "https://code.highcharts.com/stock/highcharts-more.src.js",
-    "https://code.highcharts.com/stock/modules/exporting.js",
-    "https://code.highcharts.com/stock/modules/exporting.src.js",
-    "https://code.highcharts.com/stock/modules/drilldown.js",
-    "https://code.highcharts.com/stock/modules/drilldown.src.js",
-    "https://code.highcharts.com/stock/modules/solid-gauge.js",
-    "https://code.highcharts.com/stock/modules/solid-gauge.src.js"
+STOCK_INCLUDE_LIBS = INCLUDE_LIBS + [
+    'https://code.highcharts.com/stock/modules/stock.js',
+    'https://code.highcharts.com/stock/modules/heikinashi.js',
+    'https://code.highcharts.com/stock/modules/hollowcandlestick.js',
+    'https://code.highcharts.com/stock/indicators/indicators-all.js',
+    'https://code.highcharts.com/modules/price-indicator.js',
+    'https://code.highcharts.com/modules/full-screen.js',
+    'https://code.highcharts.com/modules/stock-tools.js',
 ]
 
 
-STOCK_INCLUDE_STR = """<script src="https://code.highcharts.com/stock/highstock.js"/>
-<script src="https://code.highcharts.com/stock/highstock.src.js"/>
-<script src="https://code.highcharts.com/stock/highcharts-more.js"/>
-<script src="https://code.highcharts.com/stock/highcharts-more.src.js"/>
-<script src="https://code.highcharts.com/stock/modules/exporting.js"/>
-<script src="https://code.highcharts.com/stock/modules/exporting.src.js"/>
-<script src="https://code.highcharts.com/stock/modules/drilldown.js"/>
-<script src="https://code.highcharts.com/stock/modules/drilldown.src.js"/>
-<script src="https://code.highcharts.com/stock/modules/solid-gauge.js"/>
-<script src="https://code.highcharts.com/stock/modules/solid-gauge.src.js"/>"""
+STOCK_INCLUDE_STR = INCLUDE_STR + """
+    <script src="https://code.highcharts.com/stock/modules/stock.js"/>
+    <script src="https://code.highcharts.com/stock/modules/heikinashi.js"/>
+    <script src="https://code.highcharts.com/stock/modules/hollowcandlestick.js"/>
+    <script src="https://code.highcharts.com/stock/indicators/indicators-all.js"/>
+    <script src="https://code.highcharts.com/modules/price-indicator.js"/>
+    <script src="https://code.highcharts.com/modules/full-screen.js"/>
+    <script src="https://code.highcharts.com/modules/stock-tools.js"/>
+"""
