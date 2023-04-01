5301bf197ea8b9c349185e4fffc5370a65411a69
Chris Modzelewski <chris@highchartspython.com>
2023-04-01 14:30:53 -0400

    Added Stock Tools and related CSS to include lists.

--- highcharts_stock/constants.py
+++ highcharts_stock/constants.py
@@ -1,15 +1,18 @@
 """Defines a set of constants that are used throughout the library."""
 from highcharts_core.constants import *
 
-STOCK_INCLUDE_LIBS = INCLUDE_LIBS + [
+STOCK_INCLUDE_LIBS = [x for x in INCLUDE_LIBS]
+STOCK_INCLUDE_LIBS.extend([
+    'https://code.highcharts.com/css/stocktools/gui.css',
+    'https://code.highcharts.com/css/annotations/popup.css',
     'https://code.highcharts.com/stock/modules/stock.js',
     'https://code.highcharts.com/stock/modules/heikinashi.js',
     'https://code.highcharts.com/stock/modules/hollowcandlestick.js',
     'https://code.highcharts.com/stock/indicators/indicators-all.js',
     'https://code.highcharts.com/modules/price-indicator.js',
     'https://code.highcharts.com/modules/full-screen.js',
-    'https://code.highcharts.com/modules/stock-tools.js',
-]
+    'https://code.highcharts.com/modules/stock-tools.js'
+])
 
 
 STOCK_INCLUDE_STR = INCLUDE_STR + """
@@ -20,4 +23,15 @@ STOCK_INCLUDE_STR = INCLUDE_STR + """
     <script src="https://code.highcharts.com/modules/price-indicator.js"/>
     <script src="https://code.highcharts.com/modules/full-screen.js"/>
     <script src="https://code.highcharts.com/modules/stock-tools.js"/>
+"""
+
+
+STOCK_TOOLS_INCLUDE_LIBS = [
+    'https://code.highcharts.com/css/stocktools/gui.css',
+    'https://code.highcharts.com/css/annotations/popup.css',
+]
+
+STOCK_TOOLS_INCLUDE_STR = """
+    <link rel="stylesheet" type="text/css" href="https://code.highcharts.com/css/stocktools/gui.css">
+    <link rel="stylesheet" type="text/css" href="https://code.highcharts.com/css/annotations/popup.css">
 """
